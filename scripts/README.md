a5daec8abbdd4d9e8a83da84060bfb8edcb0ecc8
Konrad (PC) <konrad@guzek.uk>
2024-12-19 00:00:13 +0100

    add database schema scripts

---
+++ scripts/README.md
@@ -0,0 +1,6 @@
+# Database schema creation and population
+
+Execute the scripts in the following order:
+
+1. create_schema.sql
+2. create_user.sql
---
+++ scripts/create_user.sql
@@ -0,0 +1,6 @@
+-- Feel free to change the username as well as the password given to it
+CREATE USER 'liveseries_api' @'localhost' IDENTIFIED BY 'CHANGE_THIS_PASSWORD';
+-- In an ideal world, we should strictly provide the grants the API has access
+-- to a specific table rather than all the privileges.
+GRANT ALL PRIVILEGES ON liveseries.* TO 'liveseries_api' @'localhost';
+FLUSH PRIVILEGES;
