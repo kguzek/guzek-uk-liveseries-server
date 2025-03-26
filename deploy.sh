5b82389da6c0097fc68a035ea258a03445e846a6
Konrad Guzek <konrad@guzek.uk>
2025-03-26 19:26:57 +0100

    optimise build script

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,22 @@ NAMESPACE='liveseries'
 for dockerfile in ./Dockerfile.*; do
   repository=${dockerfile#./Dockerfile.}
   tag="$NAMESPACE/$repository:latest"
-  docker build -t "$tag" -f "$dockerfile" .
-  docker push "$tag"
+  # DOCKER_TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$NAMESPACE/$repository:pull" | jq -r '.token')
+  # REMOTE_MANIFEST=$(curl -sH "Authorization: Bearer $DOCKER_TOKEN" "https://registry.hub.docker.com/v2/$NAMESPACE/$repository/manifests/latest" | jq -r .layers[0].digest)
+  # LOCAL_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' "$tag" | cut -d '@' -f 2)
+
+  echo "Building $repository..."
+  build_output=$(docker build -t "$tag" -f "$dockerfile" . 2>&1)
+
+  total_steps=$(grep -E '^(RUN|WORKDIR|COPY)' $dockerfile | wc -l)
+  cached_steps=$(echo "$build_output" | grep -c "CACHED")
+
+  echo -n "$cached_steps/$total_steps build steps were cached. "
+  if [[ "$cached_steps" -lt "$total_steps" ]]; then
+    echo "Pushing image..."
+    docker push "$tag"
+  else
+    echo "Skipping push."
+  fi
+  echo
 done
