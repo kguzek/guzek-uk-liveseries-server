2f9b388cbb771dc09dfbb223e95aa8c799bd1f06
Konrad Guzek <konrad@guzek.uk>
2025-05-15 20:20:00 +0200

    ci(cd): use docker buildx cache

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,20 @@ for dockerfile in ./Dockerfile.*; do
   repository=${dockerfile#./Dockerfile.}
   tag="$REGISTRY_HOSTNAME/$PROJECT_NAME/$repository:latest"
 
+  build_image() {
+    if [[ $ENABLE_BUILDX_CACHE ]]; then
+      # Used for GitHub Actions caching
+      docker buildx build \
+        --cache-from=type=local,src=/var/cache/.buildx-cache \
+        --cache-to=type=local,dest=/var/cache/.buildx-cache \
+        -t "$tag" -f "$dockerfile" .
+    else
+      docker build -t "$tag" -f "$dockerfile" . 2>&1
+    fi
+  }
+
   echo "Building $repository..."
-  build_output=$(docker build -t "$tag" -f "$dockerfile" . 2>&1)
+  build_output=$(build_image)
 
   total_steps=$(grep -E '^(RUN|WORKDIR|COPY)' $dockerfile | wc -l)
   cached_steps=$(echo "$build_output" | grep -c "CACHED")
